cf840ab628b67651bef47d2ebe4fa1f32fd6be08
Paige Godfrey <paagodfrey@gmail.com>
2020-12-15 16:36:48 -0800

    navigation without brand pre-styling

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -7,18 +7,18 @@ import './Navigation.css'
 const Navigation = () => {
   return (
     <div>
-      <Navbar expand="sm" variant="light" id="nav-style">
+      <Navbar expand="sm" variant="light" className="justify-content-end" id="nav-style">
         <Navbar.Toggle aria-controls="navigation" />
-        <Navbar.Collapse id="navigation" className="justify-content-end">
+        <Navbar.Collapse id="navigation" className="text-center justify-content-end">
           <Nav>
             <Nav.Item>
-              <NavLink className="pr-3" exact to="/">Projects</NavLink>
+              <NavLink className="px-2" exact to="/">Projects</NavLink>
             </Nav.Item>
             <Nav.Item>
-              <NavLink className="pr-3" to="/about">About</NavLink>
+              <NavLink className="px-2" to="/about">About</NavLink>
             </Nav.Item>
             <Nav.Item>
-              <NavLink to="/contact">Contact</NavLink>
+              <NavLink className="px-2" to="/contact">Contact</NavLink>
             </Nav.Item>
           </Nav>
         </Navbar.Collapse>
