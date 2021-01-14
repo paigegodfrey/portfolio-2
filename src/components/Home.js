a7a3df695dce1bc4501d824bfa8ea4f582338bbf
Paige Godfrey <paagodfrey@gmail.com>
2021-01-13 21:16:18 -0800

    draft cover - flexbox in progress

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,26 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import rainbowIMG from '../images/rainbowIMG.png';
 import "./Home.css";
 
 const Home = () => {
   return (
-    <div className="bg">
-      <div className="intro-container">
-        <div className="header">PAIGE GODFREY</div>
-        <div className="sub-header">
-          Not your average software engineer
-        </div>
-      </div>
+    <div className='bg'>
+      <Container fluid='lg' className="intro-container">
+        <Row>
+          <Col className="flex-item1">
+            <div className="header">PAIGE GODFREY</div>
+            <div className="sub-header">
+              Not your average software engineer
+          </div>
+          </Col>
+          <Col className="flex-item2">
+            <img src={rainbowIMG} alt="geometric art" />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
