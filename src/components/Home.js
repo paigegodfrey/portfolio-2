7016d7da3429866e83cca0b85c612276ac87eb1c
Paige Godfrey <paagodfrey@gmail.com>
2021-01-15 07:47:12 -0800

    title img set

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import rubixCube from '../images/rubixCube.png';
+import diamondArt from '../images/diamondArt.png';
 import "./Home.css";
 
 const Home = () => {
@@ -7,12 +7,12 @@ const Home = () => {
     <div id="hero" className="d-flex align-items-center">
       <div className="container">
         <div className="row">
-          <div className="title col-lg-8">
+          <div className="title-text col-lg-7">
             <h1>Paige Godfrey</h1>
             <h2>Not your average software engineer</h2>
           </div>
-          <div className="col-lg-4">
-            <img className="title-img" src={rubixCube} alt="rubix cube" />
+          <div className="col-lg-5">
+            <img className="title-img" src={diamondArt} alt="geometric pic" />
           </div>
         </div>
       </div>
