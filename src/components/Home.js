cc81fab85eaf1db4828f2f0008656e67db25dca3
Paige Godfrey <paagodfrey@gmail.com>
2021-01-14 18:54:30 -0800

    landing page updated with media queries

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,26 +1,13 @@
 import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import rainbowIMG from '../images/rainbowIMG.png';
 import "./Home.css";
 
 const Home = () => {
   return (
-    <div className='bg'>
-      <Container fluid='lg' className="intro-container">
-        <Row>
-            <Col className="flex-item1">
-              <div className="header">PAIGE GODFREY</div> 
-              <div className="sub-header">
-                Not your average software engineer
-              </div>
-            </Col>
-          {/* <Col className="flex-item2">
-            <img src={rainbowIMG} alt="geometric art" />
-          </Col> */}
-        </Row>
-      </Container>
+    <div id="hero" className="d-flex align-items-center">
+      <div className="container">
+        <h1>Paige Godfrey</h1>
+        <h2>Not your average software engineer</h2>
+      </div>
     </div>
   );
 }
