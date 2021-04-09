43c5e86066466a27672fdafcb2fb6578b04d8001
Paige Godfrey <paagodfrey@gmail.com>
2021-04-08 19:26:35 -0700

    update jobly-record

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -7,12 +7,12 @@ import './Navigation.css'
 const Navigation = () => {
   return (
     <div>
-      <Navbar expand="md" variant="dark" id="nav-style">
+      <Navbar expand="md" variant="dark" className="Navigation">
         <Navbar.Brand href="/" id="nav-brand">PG</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
           <Nav>
-          <Nav.Item>
+            <Nav.Item>
               <NavLink className="home pr-3" exact to="/">Home</NavLink>
             </Nav.Item>
             <Nav.Item>
