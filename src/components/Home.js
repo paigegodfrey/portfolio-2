a2973da703b06a266e503e2441d4342a879b9996
Paige Godfrey <paagodfrey@gmail.com>
2021-04-21 22:25:14 -0700

    navigation expand fixed and  moved right

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,6 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { Link } from 'react-router-dom';
-import titleIMG from '../images/titleIMG.png';
 import "./Home.css";
 
 const Home = () => {
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -7,22 +7,22 @@ import './Navigation.css'
 const Navigation = () => {
   return (
     <div>
-      <Navbar expand="lg" variant="dark" id="nav-style">
+      <Navbar expand="md" variant="dark" id="nav-style">
         <Navbar.Brand href="/" id="nav-brand">PG</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+        <Navbar.Collapse id="basic-navbar-nav" className="text-right justify-content-end">
           <Nav>
             <Nav.Item>
-              <NavLink className="home pr-3" exact to="/">Home</NavLink>
+              <NavLink className="home p-2" exact to="/">Home</NavLink>
             </Nav.Item>
             <Nav.Item>
-              <NavLink className="projects pr-3" exact to="/projects">Projects</NavLink>
+              <NavLink className="projects p-2" exact to="/projects">Projects</NavLink>
             </Nav.Item>
             <Nav.Item>
-              <NavLink className="about pr-3" exact to="/about">About</NavLink>
+              <NavLink className="about p-2" exact to="/about">About</NavLink>
             </Nav.Item>
             <Nav.Item>
-              <NavLink className="contact" exact to="/contact">Contact</NavLink>
+              <NavLink className="contact p-2" exact to="/contact">Contact</NavLink>
             </Nav.Item>
           </Nav>
         </Navbar.Collapse>
