152e067fa9b4cdc13617228c4836c19c3779c3dd
Paige Godfrey <paagodfrey@gmail.com>
2021-04-27 14:58:18 -0700

    remove react-bootstrap from Navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,30 +1,53 @@
 import React from "react";
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
-import { NavLink } from 'react-router-dom';
+// import Navbar from 'react-bootstrap/Navbar';
+// import Nav from 'react-bootstrap/Nav';
+import { Link, NavLink } from 'react-router-dom';
 import './Navigation.css'
 
 const Navigation = () => {
   return (
-    <div>
-      <Navbar expand="md" variant="dark" id="nav-style">
-        <Navbar.Brand href="/" id="nav-brand">PG</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav" className="text-right justify-content-end">
-          <Nav>
-            <Nav.Item>
-              <NavLink className="projects p-2" exact to="/projects">Projects</NavLink>
-            </Nav.Item>
-            <Nav.Item>
-              <NavLink className="about p-2" exact to="/about">About</NavLink>
-            </Nav.Item>
-            <Nav.Item>
-              <NavLink className="contact p-2" exact to="/contact">Contact</NavLink>
-            </Nav.Item>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+    <div id="nav-style">
+      <nav className="Navigation navbar navbar-expand-md">
+        <Link className="navbar-brand" id="nav-brand" to="/">
+          PG
+        </Link>
+        <ul className="navbar-nav ml-auto" id="basic-navbar-nav">
+          <li className="nav-item mr-2">
+            <NavLink className="projects nav-link" to="/projects">
+              Projects
+            </NavLink>
+          </li>
+          <li className="nav-item mr-2">
+            <NavLink className="about nav-link" to="/about">
+              About
+            </NavLink>
+          </li>
+          <li className="nav-item mr-2">
+            <NavLink className="contact nav-link" to="/contact">
+              Contact
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
     </div>
+
+    /* <Navbar expand="md" variant="dark" id="nav-style">
+      <Navbar.Brand exact to="/" id="nav-brand">PG</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav" className="text-right justify-content-end">
+        <Nav>
+          <Nav.Item>
+            <NavLink className="projects p-2" exact to="/projects">Projects</NavLink>
+          </Nav.Item>
+          <Nav.Item>
+            <NavLink className="about p-2" exact to="/about">About</NavLink>
+          </Nav.Item>
+          <Nav.Item>
+            <NavLink className="contact p-2" exact to="/contact">Contact</NavLink>
+          </Nav.Item>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar> */
   )
 }
 
