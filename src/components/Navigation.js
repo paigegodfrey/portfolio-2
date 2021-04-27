dd64be403a542495747a6e010e031266a4649a5b
Paige Godfrey <paagodfrey@gmail.com>
2021-04-27 15:22:17 -0700

    simple Navigation - no collapse

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,13 +1,11 @@
 import React from "react";
-// import Navbar from 'react-bootstrap/Navbar';
-// import Nav from 'react-bootstrap/Nav';
 import { Link, NavLink } from 'react-router-dom';
 import './Navigation.css'
 
 const Navigation = () => {
   return (
     <div id="nav-style">
-      <nav className="Navigation navbar navbar-expand-md">
+      <nav className="Navigation navbar navbar-expand">
         <Link className="navbar-brand" id="nav-brand" to="/">
           PG
         </Link>
@@ -30,24 +28,6 @@ const Navigation = () => {
         </ul>
       </nav>
     </div>
-
-    /* <Navbar expand="md" variant="dark" id="nav-style">
-      <Navbar.Brand exact to="/" id="nav-brand">PG</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav" className="text-right justify-content-end">
-        <Nav>
-          <Nav.Item>
-            <NavLink className="projects p-2" exact to="/projects">Projects</NavLink>
-          </Nav.Item>
-          <Nav.Item>
-            <NavLink className="about p-2" exact to="/about">About</NavLink>
-          </Nav.Item>
-          <Nav.Item>
-            <NavLink className="contact p-2" exact to="/contact">Contact</NavLink>
-          </Nav.Item>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar> */
   )
 }
 
