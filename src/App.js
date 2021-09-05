432b7ffd94cbae352fa4dff7e563a2dab8002991
Paige Godfrey <paagodfrey@gmail.com>
2021-09-05 16:03:43 -0700

    font edits

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Navigation from './components/Navigation';
 import Routes from './components/Routes';
-import './App.css';
 
 const App = () => {
   return (
