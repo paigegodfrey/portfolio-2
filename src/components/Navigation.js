1a102f9b0674bc700b47f7ebfda232eb8b1d9733
Paige Godfrey <paagodfrey@gmail.com>
2021-09-05 14:55:59 -0700

    navigation and hero edits

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,29 +1,34 @@
 import React from "react";
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
 import { Link, NavLink } from 'react-router-dom';
 import './Navigation.css'
 
 const Navigation = () => {
   return (
     <div>
-      <Navbar collapseOnSelect expand="md" variant="dark" id="nav-style">
-        <Link to="/" id="nav-brand">PG</Link>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav" className="text-right justify-content-end">
-          <Nav>
-            <Nav.Item>
-              <Nav.Link className="projects mr-2" eventKey="1" as={NavLink} to="/projects">Projects</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link className="about mr-2" eventKey="2" as={NavLink} to="/about">About</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link className="contact mr-2" eventKey="3" as={NavLink} to="/contact">Contact</Nav.Link>
-            </Nav.Item>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+      <div id="nav-style">
+        <nav className="Navigation navbar navbar-expand">
+          <Link className="navbar-brand" id="nav-brand" to="/">
+            PG
+          </Link>
+          <ul className="navbar-nav ml-auto" id="basic-navbar-nav">
+            <li className="nav-item mr-2">
+              <NavLink className="projects nav-link" to="/projects">
+                Projects
+              </NavLink>
+            </li>
+            <li className="nav-item mr-2">
+              <NavLink className="about nav-link" to="/about">
+                About
+              </NavLink>
+            </li>
+            <li className="nav-item mr-2">
+              <NavLink className="contact nav-link" to="/contact">
+                Contact
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </div>
   )
 }
