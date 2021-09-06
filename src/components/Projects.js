789d66b3889c1348c57c0e785dffae6750fcff2a
Paige Godfrey <paagodfrey@gmail.com>
2021-09-05 23:00:52 -0700

    unused variables

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
-import ProjectCard from './ProjectCard';
-import jobly from '../images/jobly.jpg';
+// import ProjectCard from './ProjectCard';
+// import jobly from '../images/jobly.jpg';
 import "./Projects.css";
 
 const Projects = () => {
