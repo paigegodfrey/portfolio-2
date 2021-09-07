03e3f17193170402fde3171ddc1fb4987ba90bd5
Paige Godfrey <paagodfrey@gmail.com>
2021-09-07 01:45:03 -0700

    removed warbler

--- src/components/projectList.js
+++ src/components/projectList.js
@@ -1,55 +1,36 @@
 import jobly from "../images/jobly.png";
 import microblog from "../images/microblog.png";
-import flixster from "../images/flixster2.png";
-import warbler from "../images/warbler.png";
+import flixster from "../images/flixster.png";
 
 let projects = [
   {
     title: "Jobly",
-    technologies: "React • Node/Express • PostgreSQL",
     description: `Jobly is a full-stack mock job-search application in which users can login, view companies/jobs,
                   and apply to open positions. The backend features a RESTful API with token-based authentication
                   and the frontend uses React Context to manage global state. Integration tests are written with Jest
                   and SuperTest.`,
-    utilizes: "Utilizes: React, React Context, Node, Express, JSON Web Tokens, Bcrypt, Jest, SuperTest, PostgreSQL.",
+    technologies: "Utilizes: React, React Context, Node, Express, JSON Web Tokens, Bcrypt, Jest, SuperTest, PostgreSQL.",
     img: jobly,
     link: "http://jobly-build.surge.sh/",
     github: "https://github.com/paigegodfrey/jobly"
   },
   {
     title: "Microblog",
-    technologies: "React • Redux • Node/Express • PostgreSQL",
     description: `Microblog is a full-stack blogging application that allows users to create blog posts, add comments,
                   and rank posts by votes. Redux Thunk is used to execute asynchronous API calls, with blog titles and
                   posts stored in separate data structures to optimize loading.`,
-    utilizes: "Utilizes: React, React Hooks, Redux, Redux Thunk, Bootstrap, Node, Express, and PostgreSQL.",
+    technologies: "Utilizes: React, React Hooks, Redux, Redux Thunk, Bootstrap, Node, Express, and PostgreSQL.",
     img: microblog,
     link: "http://microblog.surge.sh/",
     github: "https://github.com/paigegodfrey/microblog"
   },
   {
     title: "Flixster",
-    technologies: "React • Node/Express • PostgreSQL",
-    description: `Jobly is a full-stack mock job-search application in which users can login, view companies/jobs,
-                  and apply to open positions. The backend features a RESTful API with token-based authentication
-                  and the frontend uses React Context to manage global state. Integration tests are written with Jest
-                  and SuperTest.`,
-    utilizes: "Utilizes: React, React Context, Node, Express, JSON Web Tokens, Bcrypt, Jest, SuperTest, PostgreSQL.",
+    description: "",
+    technologies: "",
     img: flixster,
-    link: "http://jobly-build.surge.sh/",
-    github: "https://github.com/paigegodfrey/jobly"
-  },
-  {
-    title: "Warbler",
-    technologies: "React • Node/Express • PostgreSQL",
-    description: `Jobly is a full-stack mock job-search application in which users can login, view companies/jobs,
-                  and apply to open positions. The backend features a RESTful API with token-based authentication
-                  and the frontend uses React Context to manage global state. Integration tests are written with Jest
-                  and SuperTest.`,
-    utilizes: "Utilizes: React, React Context, Node, Express, JSON Web Tokens, Bcrypt, Jest, SuperTest, PostgreSQL.",
-    img: warbler,
-    link: "http://jobly-build.surge.sh/",
-    github: "https://github.com/paigegodfrey/jobly"
+    link: "",
+    github: ""
   },
 ]
 
