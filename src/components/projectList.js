1d1d75ebe9c527b8f644c1fea38e45d0cdf82fbb
Paige Godfrey <paagodfrey@gmail.com>
2021-09-07 22:08:16 -0700

    projectList text edits

--- src/components/projectList.js
+++ src/components/projectList.js
@@ -6,11 +6,12 @@ let projects = [
   {
     title: "Jobly",
     technologies: "React • Node • Express • PostgreSQL",
-    description: `Jobly is a fullstack mock job-search application in which users can login, view companies/jobs,
-                  and apply to open positions.`,
-    techDetail:`The backend features a RESTful API with token-based authentication
-                and the frontend uses React Context to manage global state. Integration tests are written with Jest
-                and SuperTest.`,
+    description: `Jobly is a full stack mock job-search application in which 
+                  users can login, view companies/jobs, and apply to open 
+                  positions.`,
+    techDetail:`The frontend uses React Context to manage global state and the 
+                backend features a RESTful API with token-based authentication. 
+                Integration tests are written with Jest and SuperTest.`,
     img: jobly,
     link: "http://jobly-build.surge.sh/",
     github: "https://github.com/paigegodfrey/jobly"
@@ -18,10 +19,11 @@ let projects = [
   {
     title: "Microblog",
     technologies: "React • Redux • Node • Express • PostgreSQL",
-    description: `Microblog is a fullstack blogging application that allows users to create blog posts, add comments,
-                  and rank posts by votes.`,
-    techDetail: `Redux Thunk is used to execute asynchronous API calls, with blog titles and
-                posts stored in separate data structures to optimize loading.`,
+    description: `Microblog is a full stack blogging application that allows users 
+                  to create blog posts, add comments, and rank posts by votes.`,
+    techDetail:`Redux Thunk is used to execute asynchronous API calls, with blog 
+                titles and posts stored in separate data structures to optimize 
+                loading.`,
     img: microblog,
     link: "http://microblog.surge.sh/",
     github: "https://github.com/paigegodfrey/microblog"
@@ -29,9 +31,11 @@ let projects = [
   {
     title: "Flixster",
     technologies: "Vanilla JavaScript • HTML • CSS",
-    description: `Flixster is a movie application written in Vanilla JavaScript in which users can search titles and 
-                  view movie details, including genre, duration, rating, and description.`,
-    techDetail: "Data is sourced from The Movie Database API.",
+    description: `Flixster is a Vanilla JavaScript movie application in which users 
+                  can search titles and view movie details, including genre, duration, 
+                  rating, and description.`,
+    techDetail: `Application is built with pure CSS and features flexbox and grid. Data 
+                  is sourced from The Movie Database API.`,
     img: flixster,
     link: "https://flixster.netlify.app/",
     github: "https://github.com/paigegodfrey/flixster"
