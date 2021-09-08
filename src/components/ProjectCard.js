7e61763a3e1e5f23f4585098e531e85c3f0d0bdf
Paige Godfrey <paagodfrey@gmail.com>
2021-09-07 19:30:43 -0700

    project images resized

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,14 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Modal from "react-bootstrap/Modal";
 import Card from 'react-bootstrap/Card';
 import "./ProjectCard.css"
 
 const ProjectCard = project => {
-  let { img } = project.project;
+  const [showModal, setShowModal] = useState(false);
+
+  const handleClose = () => setShowModal(false);
+  const handleShow = () => setShowModal(true);
+
+  let { title, description, technologies, img, link, github } = project.project;
 
   return (
-    <Card className="ProjectCard card" style={{ border: "none" }}>
-      <Card.Img className="card-img" variant="top" src={img} />
-    </Card>
+    <>
+      <Modal show={showModal} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>
+            <h2 className="text-center">{title}</h2>
+          </Modal.Title>
+        </Modal.Header>
+        <Card>
+          <Card.Body>
+            <img
+              style={{ maxWidth: "100%" }}
+              src={img}
+              alt={`pic of ${title}`}
+              className="m-0 modal-pic"
+            />
+
+            <p className="mt-4">{description}</p>
+            <p className="mt-4">{technologies}</p>
+            {/* <p className="text-white">
+              <b className="light-blue-text">Front end:  </b>
+              {project.frontend}
+            </p>
+            <p className="text-white">
+              <b className="light-blue-text">Back end: </b>
+              {project.backend}
+            </p> */}
+            <div className="text-center">
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href={github}
+              >
+                Github
+              </a>{" "}
+              |{" "}
+              <a target="_blank" rel="noreferrer" href={link}>
+                Live Demo
+              </a>
+            </div>
+          </Card.Body>
+        </Card>
+      </Modal>
+
+      <Card onClick={handleShow} className="ProjectCard card-hover" style={{ border: "none" }}>
+        <Card.Img className="card-img" variant="top" src={img} />
+      </Card>
+    </>
+
   );
 }
 
--- src/components/projectList.js
+++ src/components/projectList.js
@@ -1,14 +1,12 @@
-import jobly from "../images/jobly.png";
-import microblog from "../images/microblog.png";
-import flixster from "../images/flixster.png";
+import jobly from "../images/jobly1.png";
+import microblog from "../images/microblog1.png";
+import flixster from "../images/flixster1.png";
 
 let projects = [
   {
     title: "Jobly",
-    description: `Jobly is a full-stack mock job-search application in which users can login, view companies/jobs,
-                  and apply to open positions. The backend features a RESTful API with token-based authentication
-                  and the frontend uses React Context to manage global state. Integration tests are written with Jest
-                  and SuperTest.`,
+    description: `Jobly is a fullstack mock job-search application in which users can login, view companies/jobs,
+                  and apply to open positions.`,
     technologies: "Utilizes: React, React Context, Node, Express, JSON Web Tokens, Bcrypt, Jest, SuperTest, PostgreSQL.",
     img: jobly,
     link: "http://jobly-build.surge.sh/",
@@ -16,9 +14,8 @@ let projects = [
   },
   {
     title: "Microblog",
-    description: `Microblog is a full-stack blogging application that allows users to create blog posts, add comments,
-                  and rank posts by votes. Redux Thunk is used to execute asynchronous API calls, with blog titles and
-                  posts stored in separate data structures to optimize loading.`,
+    description: `Microblog is a fullstack blogging application that allows users to create blog posts, add comments,
+                  and rank posts by votes.jobly`,
     technologies: "Utilizes: React, React Hooks, Redux, Redux Thunk, Bootstrap, Node, Express, and PostgreSQL.",
     img: microblog,
     link: "http://microblog.surge.sh/",
